11b7c4ee40188569814b72533968050b3f81e857
xavimagine <sr.xavier5@gmail.com>
2022-09-29 14:18:42 +0200

    sprite animated with 1type only

--- js/index.js
+++ js/index.js
@@ -24,7 +24,26 @@ const typeBakcground = {
     steel:'../librery/background/roca.png',
     fighting:'../librery/background/',
     default:'../librery/background/default.png',
-}
+};
+const typeColors = {
+    electric: '#FFEA70',
+    normal: '#B09398',
+    fire: '#FF675C',
+    water: '#0596C7',
+    ice: '#AFEAFD',
+    rock: '#999799',
+    flying: '#7AE7C7',
+    grass: '#4A9681',
+    psychic: '#FFC6D9',
+    ghost: '#561D25',
+    bug: '#A2FAA3',
+    poison: '#795663',
+    ground: '#D2B074',
+    dragon: '#DA627D',
+    steel: '#1D8A99',
+    fighting: '#2F2F2F',
+    default: '#2A1A1F',
+};
 
 
 const searchPokemon = event => {
@@ -37,11 +56,34 @@ const searchPokemon = event => {
 }
 
 const renderPokemonData = data => {
-    const sprite =  data.sprites.other.dream_world.front_default;
+    const sprite =  data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
     const { stats, types } = data;
-console.log(data)
+    console.log(types)
     // Recogemos los valores del data
     pokeName.textContent =data.name;
     pokeImg.setAttribute('src',sprite);
     pokeId.textContent= `Nº${data.id}`;
+    renderPokemonType(types);
 }
+// Cambiar el fondo segun los tipos
+// const setBackground = types =>{
+//     // const oneType =
+// }
+// types de los pokemons
+const renderPokemonType = types =>{
+    pokeTypes.innerHTML='';
+    // resetealos tipos para una nueva busqueda
+    types.forEach(type => {
+        const typeTextElement = document.createElement("div");
+        console.log(typeTextElement)
+        typeTextElement.style.color = typeColors[type.type.name];
+        typeTextElement.textContent= type.type.name;
+        pokeTypes.appendChild(typeTextElement);
+        console.log(poke)
+    });
+}
+
+
+// function cambiarFondo){
+//     document.getElementById("fondo").src="image2.jpg";
+//   }
